0d692c3d5d3f8da332bf735da0e19189304295e3
mauriciosc <ninesixblufs@gmail.com>
2020-01-14 11:41:06 -0300

    ajust slash

--- app/src/main/java/com/example/tela/tela2.java
+++ app/src/main/java/com/example/tela/tela2.java
@@ -2,7 +2,9 @@ package com.example.tela;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class tela2 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class tela2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
+    }
+    public void tela3map (View view){
+
+        Intent intent = new Intent(this, tela3.class);
+        startActivity(intent);
     }
 }
