3e0469375cd251009a376268a08ffda97e0cb0a5
Macaco6 <ninesixblufs@gmail.com>
2020-01-29 09:41:02 -0200

    29-01-2020- implemetation web Service

--- app/src/main/java/com/example/tela/busca.java
+++ app/src/main/java/com/example/tela/busca.java
@@ -3,6 +3,10 @@ package com.example.tela;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class busca extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class busca extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busca);
+
+        final EditText etCep = findViewById(R.id.etMain_cep);
+
+        Button btnBuscarCep = findViewById(R.id.btnMain_buscarCep);
+        btnBuscarCep.setOnClickListener(new View.OnClickListener());
+        {
+            public void onClick (View busca){
+            if (etCep.getText().toString().length() > 0 && !etCep.getText().toString().equals(" ") && etCep.getText().toString().length() == 8) {
+                Log.i("teste", "CEP valido");
+
+
+            }
+        }
+            ;
+        }
+
     }
+
 }
